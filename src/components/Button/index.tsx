0d990b064dbbbb6504d6dc43ce9dcbb34386b192
Diou Medeiros <diou.182@gmail.com>
2020-10-19 21:56:34 -0300

    feat: Created Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,12 @@
 import { Wrapper } from './styles';
 
-const Button = () => (
-  <Wrapper>
-    <h1>Button</h1>
-  </Wrapper>
+export type ButtonProps = {
+  children?: React.ReactNode;
+  size?: 'small' | 'medium' | 'large';
+};
+
+const Button = ({ children, size = 'medium' }: ButtonProps) => (
+  <Wrapper size={size}>{!!children && <span>{children}</span>}</Wrapper>
 );
 
 export default Button;
