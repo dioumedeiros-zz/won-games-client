952c09549cb39da8d2c1b7c5104e2ee25b7d56a8
Diou Medeiros <diou.182@gmail.com>
2020-10-28 22:59:44 -0300

    feat: Created Icon option in Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,10 +4,19 @@ export type ButtonProps = {
   children?: React.ReactNode;
   size?: 'small' | 'medium' | 'large';
   fullWidth?: boolean;
+  icon?: React.ReactNode;
+  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const Button = ({ children, size = 'medium', fullWidth }: ButtonProps) => (
-  <Wrapper size={size} fullWidth={fullWidth}>
+const Button = ({
+  children,
+  size = 'medium',
+  fullWidth,
+  icon,
+  ...props
+}: ButtonProps) => (
+  <Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+    {!!icon && icon}
     {!!children && <span>{children}</span>}
   </Wrapper>
 );
