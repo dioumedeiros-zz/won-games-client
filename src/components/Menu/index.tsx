263b83aeca8c9063846c169690aaca0a50a5539c
Diou Medeiros <diou.182@gmail.com>
2020-12-29 17:37:39 -0300

    feat: Created open menu logic

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,29 +1,39 @@
+import { useState } from 'react';
 import { Menu2 as MenuIcon } from 'styled-icons/remix-fill';
 import {
   ShoppingCart as ShoppingCartIcon,
   Search as SearchIcon,
+  Close as CloseIcon,
 } from 'styled-icons/material-outlined';
 
 import Logo from '../Logo';
 import * as S from './styles';
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo hideOnMobile />
-    </S.LogoWrapper>
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <SearchIcon aria-label="search" />
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open Menu" />
       </S.IconWrapper>
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="open Shopping Cart" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-);
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="open Shopping Cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  );
+};
 
 export default Menu;
